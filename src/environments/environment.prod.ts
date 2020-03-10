b9f2d54dc166f4ce7e2ac44b47d5ee57a35c7190
Ivan Kupchyk <ivan.kupchic@gmail.com>
2020-03-10 18:47:09 +0300

    Added env to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAVFCAAr_SwSd3OzRg0S7_Ta_5TKq3ymHY',
+    authDomain: 'new-cars-70cbe.firebaseapp.com',
+    databaseURL: 'https://new-cars-70cbe.firebaseio.com',
+    projectId: 'new-cars-70cbe',
+    storageBucket: 'new-cars-70cbe.appspot.com',
+    messagingSenderId: '653229456243',
+    appId: '1:653229456243:web:be04b99a4adbc833c26306'
+  }
 };
